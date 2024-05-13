d8cfc284977a3c40577b4cc392c674f761befa0a
Nathalie Claire Huppert <nathalie.huppert76@gmail.com>
2024-05-13 18:14:49 +0200

    added domain/ and files inside

---
+++ src/domain/API.ts
@@ -0,0 +1,112 @@
+import { Book } from './book';
+
+// Requesting all books
+async function getAllBooks(): Promise<Book[] | undefined> {
+    try {
+        const response = await fetch('http://localhost:4730/books');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const books: Book[] = await response.json();
+        return books;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+// Requesting one book by ISBN
+async function getBookByISBN(isbn: string): Promise<Book> {
+    try {
+        const response = await fetch(`http://localhost:4730/books/${isbn}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const book: Book = await response.json();
+        return book;
+    } catch (error) {
+        console.error('Error:', error);
+        return {
+            id: "0",
+            title: "This book does not exist!",
+            subtitle: "Please check the ISBN and try again.",
+            isbn: "0",
+            abstract: "This book does not exist!",
+            author: "Unknown",
+            publisher: "Unknown",
+            price: "$0",
+            numPages: 0,
+            cover: "https://via.placeholder.com/150",
+            userId: 0
+        };
+    }
+}
+
+// Posting a new book
+async function postNewBook(book: Book): Promise<Book> {
+    try {
+        const response = await fetch('http://localhost:4730/books', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(book),
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const newBook: Book = await response.json();
+        return newBook;
+    } catch (error) {
+        console.error('Error:', error);
+        return {
+            id: "0",
+            title: "FAILED!",
+            subtitle: "Uploading the new Book FAILED",
+            isbn: "0",
+            abstract: "Not cool!",
+            author: "Unknown",
+            publisher: "Unknown",
+            price: "$0",
+            numPages: 0,
+            cover: "https://via.placeholder.com/150",
+            userId: 0
+        };
+    }
+}
+
+// Updating an existing book
+async function updateBook(isbn: string, updatedBook: Book): Promise<Book | undefined> {
+    try {
+        const response = await fetch(`http://localhost:4730/books/${isbn}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatedBook),
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const book: Book = await response.json();
+        return book;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+// Deleting a book
+async function deleteBook(isbn: number): Promise<boolean | undefined> {
+    try {
+        const response = await fetch(`http://localhost:4730/books/${isbn}`, {
+            method: 'DELETE',
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.ok;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+module.exports = { getAllBooks, getBookByISBN, postNewBook, updateBook, deleteBook };
---
+++ src/domain/APITester.ts
@@ -0,0 +1,72 @@
+const { getAllBooks, getBookByISBN, postNewBook, updateBook, deleteBook } = require('./API');
+import { Book } from './book';
+
+// Requesting all books
+async function executeGetAllBooks(): Promise<Book[]> {
+    const allBooks: Book[] = await getAllBooks();
+    console.log(allBooks);
+    return allBooks;
+}
+
+// Requesting one book by ISBN
+async function executeGetOneBook(): Promise<Book> {
+    const book: Book = await getBookByISBN("1001606140805");
+    console.log(book);
+    return book;
+}
+
+// Posting a new book
+// disclaimer: Doesn't work when the Book with the ISBN is already in the database!!
+async function executePostOneBook(): Promise<Book> {
+    const newBook = {
+        id: "12345678900",
+        title: 'Test Book',
+        subtitle: 'The best book ever!',
+        isbn: "12345678900",
+        abstract: 'Hi, this is the abstract for the example book!',
+        author: 'Nathalie Claire Huppert',
+        publisher: 'HTW Berlin',
+        price: '$38.99',
+        numPages: 3926,
+        cover: 'http://localhost:4730/covers/9781430232766.png',
+        userId: 1,
+    };
+    const postedBook: Book = await postNewBook(newBook);
+    console.log(postedBook);
+    return postedBook;
+}
+
+// Updating an existing book
+async function executeUpdateOneBook(): Promise<Book> {
+    const updatedBook = {
+        id: '12345678900',
+        title: 'Updated Book',
+        subtitle: 'Just updated the best book ever!',
+        author: 'just Nathalie',
+        isbn: '12345678900',
+        abstract: 'Hi, this is the updated abstract for the example book!',
+        publisher: 'HTW Berlin',
+        price: '$38.99',
+        numPages: 3926,
+        cover: 'http://localhost:4730/covers/9781430232766.png',
+        userId: 1,
+    };
+    const updated: Book = await updateBook("12345678900", updatedBook);
+    console.log(updated);
+    return updated;
+}
+
+// Deleting a book
+async function executeDeleteOneBook(): Promise<Book> {
+    const deleted: Book = await deleteBook("12345678900");
+    console.log(deleted ? 'Deleted successfully' : 'Delete failed');
+    return deleted;
+}
+
+// Call the functions
+
+// executeGetAllBooks();
+executeGetOneBook();
+// executePostOneBook();
+// executeUpdateOneBook();
+// executeDeleteOneBook();
