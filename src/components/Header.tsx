a9547fbbca91dac6d184f624b49eda0baa874555
Nathalie Claire Huppert <nathalie.huppert76@gmail.com>
2024-05-13 15:02:04 +0200

    changes

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hi, This is the Header!!!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
