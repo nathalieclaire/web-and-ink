81be27d1530c33ccdd45fe8cf7185f5ca9f82313
Nathalie Claire Huppert <nathalie.huppert76@gmail.com>
2024-06-20 15:53:11 +0200

    added logo_book and media queries

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,8 +6,11 @@ function Header() {
   return (
     <div className="App">
       <header className="header flex flex-c text-center">
-        <img src="/logo_text_white.png" /* Use the absolut path from root here to prevent isssues! */ 
-          className="logo"/>
+        <div className="logo-container flex flex-c">
+          <img src="/logo_text_white.png" /* Use the absolut path from root here to prevent isssues! */ 
+            className="logo"/>
+          <img src="/logo_book.png" className="logo-book"/>
+        </div>
         <div className="header-text">
           <h2>Your Virtual Bookstore</h2>
         </div>
