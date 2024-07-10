312f30ee3c3fe3a395b65ef44636a8de4230b26b
Nathalie Claire Huppert <nathalie.huppert76@gmail.com>
2024-07-10 16:42:11 +0200

    conditionally render Nav in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Header.css';
 import Nav from '../Nav/Nav';  
+import { useLocation } from 'react-router-dom';
 
 function Header() {
+  const location = useLocation();
+  const showNavBar = location.pathname !== '/login'; // only renders the Nav when the user is not on the login page
+
   return (
     <div className="App">
       <header className="header flex flex-c text-center">
@@ -14,7 +18,7 @@ function Header() {
         <div className="header-text">
           <h2>Your Virtual Bookstore</h2>
         </div>
-        <Nav />
+        {showNavBar && <Nav /> /*conditional rendering of Nav */ }
       </header>
     </div>
   );
