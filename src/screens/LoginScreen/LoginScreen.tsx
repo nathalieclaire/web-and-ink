2b0ea8660b93ebfa87d2c0302c96dcd84d18af4a
Nathalie Claire Huppert <nathalie.huppert76@gmail.com>
2024-07-10 18:46:01 +0200

    implemented login functionality

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,25 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginScreen.css';
+import { useNavigate } from 'react-router-dom';
 
 function LoginScreen() {
-    // const handleLogin() {
-        // if creadentials can't be found in the database, route back to /login
-        // else let user in!
-    // }
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('admin@bookmonkey.api');
+    const [password, setPassword] = useState('password1!');
+
+    async function handleLogin(event: React.FormEvent) {
+        event.preventDefault();
+
+        try {
+            const x =JSON.stringify({ "email": email, "password": password });
+            console.log(x);
+            const response = await fetch('http://localhost:4730/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: x,
+            });
+            console.log(response);
+            console.log(response.status);
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const res = await response.json();
+            console.log("USER:", res);
+
+            if (res) {
+                if (res.user.role === "admin") {
+                    navigate('/');
+                } else if (res.user.role === "non-admin") {
+                    navigate('/about');
+                } else {
+                    alert('Your account is not authorized.');
+                    navigate('/login');
+                }
+            }
+        } catch (error) {
+            console.error('Error during login:', error);
+            alert('The credentials you entered are incorrect!');
+            navigate('/login');
+        }
+    }
+
 
   return (
     <div className="App login-container">
         <h1 className="blue-color bold-label2 flex flex-c">Welcome to Web&Ink!</h1>
-        <form className="login-form" /*onSubmit={handleLogin}*/>
+        <form className="login-form" onSubmit={handleLogin}>
             <label>
                 <span className="bold-label">E-Mail: </span>
                 <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required
                 />
             </label>
             <label>
                 <span className="bold-label">Password: </span>
                 <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 required
                 />
             </label>
